17ff77a7eaaa48b225d422f7fcd08adc2adf2e1f
Arthur Burgin Jr <arthur.burginjr@libertymutual.com>
2021-09-26 13:00:35 -0500

    Initial Commit

---
+++ Barcode Scanner/Barcode_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barcode_ScannerApp.swift
+//  Barcode Scanner
+//
+//  Created by Arthur on 9/26/21.
+//
+
+import SwiftUI
+
+@main
+struct Barcode_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
